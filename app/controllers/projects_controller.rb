c24951ee7404dd3d77ea9e9cdced19c152800b40
okaydokay97 <rubens.fernandes97@gmail.com>
2020-06-08 10:32:41 -0500

    first init

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,41 @@
+class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+  end
+  
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def new
+    @project = Project.new()
+  end
+
+  def create
+    project = Project.create(project_params)
+    redirect_to project_path(project.id)
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    project = Project.find(params[:id])
+    project.update(project_params)
+    redirect_to project_path(project.id)
+  end
+
+  def delete
+    project = Project.find(params[:id])
+    project.delete
+    redirect_to project_path(index)
+  end
+
+      private  
+      
+      def project_params
+        params.require(:project).permit(:name, :funding_goal)
+      end
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Project.create(name: "Coolest", funding_goal: 300000000)
+Project.create(name: "batman", funding_goal: 3000)
+Project.create(name: "wowow", funding_goal: 3002340)
+Project.create(name: "nice", funding_goal: 100)
+Project.create(name: "sweet", funding_goal: 3)
